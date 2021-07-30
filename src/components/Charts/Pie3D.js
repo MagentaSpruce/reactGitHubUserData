304037ab2abe6a492113c7c5d114b3437fad876d
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-29 21:04:28 -0400

    Pie Chart
    Set up pie chart in Pie3D.js.

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,47 @@
-import React from 'react';
+// Step 1 - Include react
+import React from "react";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+// Step 2 - Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Step 3 - Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Step 4 - Include the chart type
+import Column2D from "fusioncharts/fusioncharts.charts";
+
+// Step 5 - Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "pie3d", // The chart type
+    width: "400", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: "Most commonly used language",
+        //Set the chart subcaption
+        // subCaption: "per repo",
+        // //Set the x-axis name
+        // xAxisName: "Language",
+        // //Set the y-axis name
+        // yAxisName: "Repos",
+        // numberSuffix: "",
+        //Set the theme for your chart
+        theme: "fusion",
+        decimals: 1,
+        pieRadius: "35%",
+        palleteColors: "#f0db4f, #b3f5, #f4cc,#33cc",
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Pie3D;
+export default ChartComponent;
